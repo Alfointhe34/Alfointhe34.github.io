2956a71d3f420dd02fba33e00c5ff95716767254
Alfointhe34 <akimaruriyuki@gmail.com>
2025-02-02 15:00:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+  "tenan?",
+  "tenan ga ngopi??",
+  "wes to ayo",
+  "ndang cok",
+  "epepan !",
+  "enak gok isian",
+  "ayo mbut.",
+  "atrahe jmbod",
+  "oke ws kapan kapan.",
+  "tapi boong! ❤️",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
